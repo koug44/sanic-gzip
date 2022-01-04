2ca0c654b7ba71d6797da25d29212b29328becf1
Eric Spitler <1396382+eric-spitler@users.noreply.github.com>
2022-01-04 04:04:53 +0000

    Add Sanic minimum version
    Breaking changes in `sanic 21.12` make `sanic-gzip 0.4.0` incompatible with older `Sanic` releases

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sanic_gzip",
-    version="0.3.0",
+    version="0.4.0",
     author="Damien Alexandre",
     author_email="damien.alexandre@muage.org",
     description="Add compress to Sanic response as decorator",
@@ -19,6 +19,6 @@ setuptools.setup(
     ],
     python_requires=">=3.6",
     install_requires=[
-        'sanic'
+        'sanic>=21.12'
     ]
 )
